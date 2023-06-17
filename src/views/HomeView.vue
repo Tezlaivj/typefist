cb397c3515f675cc83a5833e7aaf5253d79e5506
Michael Wayne <54175718+michaelwaynewibisono@users.noreply.github.com>
2023-06-17 18:50:31 +0700

    Update HomeView.vue

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,18 +2,24 @@
     <testCoba />
     <TypeGame />
     <ArticlePage />
+    <YoutubePage />
+    <FooterPage />
 </template>
 
 <script>
 import ArticlePage from '@/components/ArticlePage.vue';
 import testCoba from '@/components/testCoba.vue';
 import TypeGame from '@/components/TypeGame.vue'
+import YoutubePage from '@/components/youtubePage.vue'
+import FooterPage from '@/components/FooterPage.vue'
 
 export default {
     components: {
         ArticlePage,
         testCoba,
-        TypeGame
+        TypeGame,
+        YoutubePage,
+        FooterPage
     }
 }
 </script>
